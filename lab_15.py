1998142e538b4bf20bce74f9899a42941429d1c0
Alberto Garcia <albergarcia@csumb.edu>
2025-10-29 01:10:21 -0700

    Submission files

---
+++ lab_15.py
@@ -0,0 +1,20 @@
+from flask_bootstrap import Bootstrap5
+from flask import Flask, render_template
+
+app = Flask(__name__)
+bootstrap = Bootstrap5(app)
+
+# Task 2: Hello World
+@app.route('/')
+def hello_world():
+    return """
+    <h1>Hello World!</h1>
+    <p>John D. : afk</p>
+    <p>Jane S. : brb</p>
+    <p>Richard Roe. : lol</p>
+    """
+
+# Task 3: Add template
+@app.route('/alberto')
+def alberto():
+    return render_template('index.html')
